cf4d989ad1d1a62a4302d129b9e4da42531badc4
moonrailgun <moonrailgun@gmail.com>
2015-07-29 18:08:41 +0800

    基本UI与框架
    ADD 基本UI
    ADD 调试信息显示
    ADD 时间显示

---
+++ WebSocketChattingRoom/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+
+namespace WebSocketChattingRoom
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOpenWSServer(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Debug("正在启动WebSocket服务器...");
+            }
+            catch (Exception ex)
+            {
+                Debug("发生异常:" + ex.ToString());
+            }
+        }
+
+        private void OnCloseWSServer(object sender, RoutedEventArgs e)
+        {
+            Debug("正在关闭WebSocket服务器...");
+        }
+
+        private void Debug(string message)
+        {
+            DateTime time = DateTime.Now;
+
+            int index = this.MessageWindow.Items.Add(string.Format("[{0}]{1}", time.ToString("HH:mm:ss"), message));
+            this.MessageWindow.ScrollIntoView(this.MessageWindow.Items[index]);
+        }
+    }
+}
